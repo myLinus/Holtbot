3bc12ee26fb021a870350c3b9dc03a2e4a7f745b
myLinus <jonas.refby@gmail.com>
2022-02-07 19:42:22 +0100

    created loops for events and commands

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+require('dotenv').config()
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require("fs");
 
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+  })
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
 
-client.login(process.env.BOT_TOKEN);
+const files = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of files) {
+  const eventName = file.split(".")[0];
+  const event = require(`./events/${file}`);
+  client.on(eventName, event.bind(null, client));
+}
+
+const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commands) {
+  const commandName = file.split(".")[0];
+  const command = require(`./commands/${file}`);
+
+  console.log(`Attempting to load command ${commandName}`);
+  client.commands.set(command.name, command);
+}
+
+client.login(process.env.botToken);
