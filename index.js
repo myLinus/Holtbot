938f480a44da46b248254f5e3c4e234ea4ab3bb3
myLinus <jonas.refby@gmail.com>
2022-02-10 19:30:14 +0100

    update index and config sample

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require('dotenv').config()
 const Discord = require('discord.js');
+const fs = require("fs");
+const config = require(`./config.json`);
 const myIntents = new Discord.Intents(14031);
 const client = new Discord.Client({intents:myIntents});
-const fs = require("fs");
+
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -11,20 +12,17 @@ client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 client.on("debug", (e) => console.info(e));
 
-const files = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-for (const file of files) {
-  const eventName = file.split(".")[0];
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const eventFile of eventFiles) {
+  const eventName = eventFile.split(".")[0];
   const event = require(`./events/${file}`);
   client.on(eventName, event.bind(null, client));
 }
 
-const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-for (const file of commands) {
-  const commandName = file.split(".")[0];
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const commandFile of commandFiles) {
+  const commandName = commandFile.split(".")[0];
   const command = require(`./commands/${file}`);
-
-  console.log(`Attempting to load command ${commandName}`);
-  client.commands.set(command.name, command);
 }
 
-client.login(process.env.botToken)
+client.login(config.botToken);
